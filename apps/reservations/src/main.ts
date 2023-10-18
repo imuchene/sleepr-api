f7948cb4ae1eb33320d6d1b50b35c3b39acf6358
Isaiah Muchene <imuchene@msn.com>
2023-10-18 17:40:12 +0300

    - Used docker to containerize the reservations microservice - Whitelisted parameters for creating/updating a reservation

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT');
   await app.listen(port);
