656c7df3717300bb3ddd06c541651c811c469b37
Isaiah Muchene <imuchene@msn.com>
2023-10-25 17:11:10 +0300

    Created logic for logging in a user

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,12 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   async create(createUserDto: CreateUserDto) {
-    return await this.usersRepository.create(createUserDto);
+    const salt = await bcrypt.genSalt();
+
+    return await this.usersRepository.create({
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, salt),
+    });
+  }
+
+  async verifyUser(email: string, password: string) {
+    const user = await this.usersRepository.findOne({ email });
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Credentials are not valid');
+    }
+    return user;
   }
 }
