55daf6383907c7b4643080fa027f485bd5fbbd5b
Isaiah Muchene <imuchene@msn.com>
2023-11-03 19:17:36 +0300

    - Set the auth app to be a microservice - Created a common jwt auth guard

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -4,13 +4,16 @@ import { Logger } from 'nestjs-pino';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  app.connectMicroservice<MicroserviceOptions>({ transport: Transport.TCP});
   app.use(cookieParser());
   const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
+  await app.startAllMicroservices();
   await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
