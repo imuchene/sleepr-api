9939e0eb7f029f5c2a73185c9e936c4bf083ccb5
Isaiah Muchene <imuchene@msn.com>
2025-04-07 13:26:53 +0300

    Used the correct port configuration for the payments service

--- apps/payments/src/main.ts
+++ apps/payments/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     transport: Transport.TCP,
     options: {
       host: '0.0.0.0',
-      port: configService.get<number>('PORT'),
+      port: configService.get<number>('TCP_PORT'),
     },
   });
 
